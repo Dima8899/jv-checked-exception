4b45576ece7959c7cbf4bf1a01dcfd4be1229910
Dima Babenko <klapster1@gmail.com>
2025-11-28 09:35:25 +0300

    new

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,15 +3,15 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
         if (password == null || repeatPassword == null) {
-            throw new PasswordValidationException("Password or repeat password cannot be null");
+            throw new PasswordValidationException("Wrong passwords");
         }
 
         if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Passwords do not match");
+            throw new PasswordValidationException("Wrong passwords");
         }
 
         if (password.length() < 10) {
-            throw new PasswordValidationException("Password should be at least 10 characters");
+            throw new PasswordValidationException("Wrong passwords");
         }
 
     }
